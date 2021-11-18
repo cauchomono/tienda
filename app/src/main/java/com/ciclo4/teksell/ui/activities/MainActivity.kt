cc35261417ae8f99295950afc9db11a5281f52d3
cauchomono <sebasfo06@hotmail.com>
2021-11-18 00:29:57 -0500

    splash Screen y creacion inicial de pantallas (fragments)

--- app/src/main/java/com/ciclo4/teksell/ui/activities/MainActivity.kt
+++ app/src/main/java/com/ciclo4/teksell/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ciclo4.teksell
+package com.ciclo4.teksell.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ciclo4.teksell.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/ciclo4/teksell/ui/activities/SplashScreenActivity.kt
@@ -0,0 +1,42 @@
+package com.ciclo4.teksell.ui.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils.loadAnimation
+import android.widget.Toast
+import com.ciclo4.teksell.R
+import com.ciclo4.teksell.databinding.ActivitySplashScreenBinding
+import com.google.android.material.animation.AnimationUtils
+
+
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding : ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val animation = loadAnimation(this, R.anim.animation)
+        binding.imageView.startAnimation(animation)
+        val intent = Intent(this, MainActivity::class.java)
+
+        animation.setAnimationListener(object:Animation.AnimationListener{
+            override fun onAnimationStart(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+        })
+    }
+}
