e4a7a1cd539fc6bd7f8856f7faed393cd638f32f
cauchomono <sebasfo06@hotmail.com>
2021-11-18 17:30:36 -0500

    se añade el bottomNavigationView

--- app/src/main/java/com/ciclo4/teksell/ui/fragments/OrderDetailFragment.kt
+++ app/src/main/java/com/ciclo4/teksell/ui/fragments/OrderDetailFragment.kt
@@ -35,7 +35,7 @@ class OrderFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order, container, false)
+        return inflater.inflate(R.layout.fragment_order_detail, container, false)
     }
 
     companion object {
